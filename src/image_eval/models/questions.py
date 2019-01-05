ef7fe42cc7cb817d6df4d433386105fc52aac990
Vsevolod Poletaev <poletaev.va@gmail.com>
2019-01-05 18:19:31 +0300

    Add capability to add evaluation using admin panel

--- src/image_eval/models/questions.py
+++ src/image_eval/models/questions.py
@@ -1,9 +1,15 @@
 import json
+import os
+from typing import Iterable
+from zipfile import ZipFile
 
-from django.db import models
+from django.core.files.base import ContentFile
+from django.db import models, transaction
 from django.contrib import admin
 from django.db.models import QuerySet
+from django.forms import ModelForm, FileField, CharField
 from django.shortcuts import redirect
+from django.utils import timezone
 
 
 class Evaluation(models.Model):
@@ -38,20 +44,90 @@ class ImageClassificationQuestion(Question):
 
 
 class ImageSelectionQuestion(Question):
+    """
+    Example:
+          ::
+        ImageSelectionQuestion(
+            evaluation=e,
+            text='Test test',
+            order=0,
+            left_image=ContentFile(b1, 'li0.png'),
+            right_image=ContentFile(b2, 'ri0.png')
+        ).save()
+
+    """
     left_image = models.ImageField()
     right_image = models.ImageField()
 
 
-class EvaluationAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'created_at', 'type', 'total_questions']
-    ordering = ['created_at']
+class EvaluationForm(ModelForm):
+    images = FileField()
+    question = CharField(max_length=1000)
 
+
+@admin.register(Evaluation)
+class EvaluationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'type', 'total_questions', 'created_at']
+    ordering = ['created_at']
     actions = ['export_result']
+    readonly_fields = ['created_at', 'total_questions']
+    list_editable = ['title']
+    list_display_links = None
+    form = EvaluationForm
 
     def export_result(self, request, queryset: QuerySet):
         return redirect('export_result', id=queryset.all()[0].id)
 
+    @transaction.atomic
+    def save_model(self, request, obj, form: ModelForm, change):
+        if change:
+            super().save_model(request, obj, form, True)
+        else:
+            data = form.cleaned_data
+            evaluation: Evaluation = Evaluation(title=data['title'],
+                                                type=data['type'],
+                                                created_at=timezone.now(),
+                                                total_questions=0)
+            evaluation.save()
 
-admin.site.register(Evaluation, EvaluationAdmin)
+            evaluation.total_questions = self.make_selection_questions_from_zip(
+                data['images'],
+                data['question'],
+                evaluation
+            )
+
+            evaluation.save()
+
+    @staticmethod
+    def make_selection_questions_from_zip(zip_file_stream, question_text, evaluation):
+        with ZipFile(zip_file_stream) as images:
+            total_questions = 0
+            left_image_names: Iterable[str] = \
+                filter(lambda s: s.startswith('left/'), images.namelist())
+
+            for name in left_image_names:
+                fname = name.split('/')[1]
+
+                if len(fname) != 0:
+                    no = int(os.path.splitext(fname)[0]) + 1
+
+                    if no > total_questions:
+                        total_questions = no
+
+            questions = []
+            for no in range(total_questions):
+                left_image_bytes = images.read(f'left/{no}.jpg')
+                right_image_bytes = images.read(f'right/{no}.jpg')
+
+                q = ImageSelectionQuestion(
+                    evaluation=evaluation, text=question_text,
+                    order=no,
+                    left_image=ContentFile(left_image_bytes, f'{evaluation.id}_{no}_l.jpg'),
+                    right_image=ContentFile(right_image_bytes, f'{evaluation.id}_{no}_r.jpg')
+                )
+                q.save()
+                questions.append(q)
+        return total_questions
+
 
 __all__ = ['Evaluation', 'Question', 'ImageSelectionQuestion', 'ImageClassificationQuestion']
